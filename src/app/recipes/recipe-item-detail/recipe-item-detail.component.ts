8f3542d0744cfaa50af0a4bf9eae397040599277
Cameron Graham <camy117@gmail.com>
2017-09-18 09:54:10 +0100

    revisit of shopping list service

--- src/app/recipes/recipe-item-detail/recipe-item-detail.component.ts
+++ src/app/recipes/recipe-item-detail/recipe-item-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 @Component({
     selector: 'app-recipe-item-detail',
     templateUrl: './recipe-item-detail.component.html'
@@ -8,9 +10,13 @@ import { RecipeService } from '../recipe.service';
 
 export class RecipeItemDetailComponent{
     @Input() recipe: Recipe;
-    constructor(private recipeService: RecipeService) {}
+    constructor(private recipeService: RecipeService, private shoppingListSerivce: ShoppingListService) {}
 
     loadRecipe() {
         this.recipeService.recipeSelected.emit(this.recipe);
+    }
+
+    onAddToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListSerivce.addIngredients(ingredients);
     }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,24 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Recipe', 'This is not a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('Big Burger'
+        , 'You will be full after this big burger',
+        'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    [
+        new Ingredient('Beef', 100),
+        new Ingredient('Chips', 50)
+    ]),
+        new Recipe('Chicken Sandwitch',
+         'Quick and easy sandwitch',
+         'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+            new Ingredient('Buns', 2),
+            new Ingredient('Chicken Breast', 2)
+        ])
       ];
 
       getRecipes() {
